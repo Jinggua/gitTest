b076864c55534e4db71a13442377ac7e94d20e2f
Yao <sudianyao@gmail.com>
2020-11-21 17:03:47 +0800

    detail

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 function hello() {
   console.log('hello world')
 }
+
+
+/**  
+ * 1 本地新建饼切换至新建分支
+ * 2 git add  git commit
+ * 3git push的时候 分为几种情况
+ *  有本地分支,没有远程分支
+    git push origin local_branch:remote_branch   // 添加远程分支,
+ * 4 git push -u origin daily/1.0.0   //  git push -u origin remote_branch
+*/
